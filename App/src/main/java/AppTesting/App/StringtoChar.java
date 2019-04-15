3295c0ff7b15d7ae307039d15a4c7416a32ebd7c
Binay Yadav <binay.qa@gmail.com>
2019-04-15 15:04:47 -0700

    Again Commited

--- App/src/main/java/AppTesting/App/StringtoChar.java
+++ App/src/main/java/AppTesting/App/StringtoChar.java
@@ -5,7 +5,10 @@ public class StringtoChar {
 	public static void main(String[] args) {
 		String s1="Binay Kumar";
 		char ch=s1.charAt(0);
-		System.out.println(ch);
+		int len=s1.length();
+		System.out.println(len);
+		for(int i=0;i<len;i++)
+		System.out.println(s1.charAt(i));
 		
 	}
 
