a6743a77e666de809387857d2e83e76687caca6f
Binay Yadav <binay.qa@gmail.com>
2019-04-15 14:25:05 -0700

    Added first project ever on github

---
+++ App/src/main/java/AppTesting/App/GoogleLaunch.java
@@ -0,0 +1,32 @@
+package AppTesting.App;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GoogleLaunch {
+
+	private static WebDriverWait WebDriverWait;
+
+	public static void main(String[] args) {
+				
+		WebDriver driver;
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\BinayBaba\\AppData\\Local\\Temp\\chromedriver.exe");
+	driver=new ChromeDriver();
+		
+		driver.get("https://www.seleniumhq.org/projects/webdriver/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		WebDriverWait =new WebDriverWait(driver,10);
+		driver.findElement(By.xpath("//a[@title='Get Selenium']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'About Selenium')]")).click();
+		
+		driver.close();
+
+	}
+
+}
