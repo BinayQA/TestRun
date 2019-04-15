119f6fd71c9ad3b96953802812883e53057012e3
Binay Yadav <binay.qa@gmail.com>
2019-04-15 14:43:31 -0700

    Commited

---
+++ App/src/main/java/AppTesting/App/StringtoChar.java
@@ -0,0 +1,12 @@
+package AppTesting.App;
+
+public class StringtoChar {
+
+	public static void main(String[] args) {
+		String s1="Binay Kumar";
+		char ch=s1.charAt(0);
+		System.out.println(ch);
+		
+	}
+
+}
